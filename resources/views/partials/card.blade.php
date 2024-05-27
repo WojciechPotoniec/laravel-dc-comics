f678834c2a85bf07895e47ceb9a436cccd4caa89
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-27 19:42:29 +0200

    created card

---
+++ resources/views/partials/card.blade.php
@@ -0,0 +1,8 @@
+<div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-4">
+    <div class="wp-card-img">
+        <img src="{{ $card['thumb'] }}" class="card-img-top" alt="{{ $card['title'] }}" />
+    </div>
+    <div class="card-body">
+        <h5 class="card-text">{{ $card['title'] }}</h5>
+    </div>
+</div>
