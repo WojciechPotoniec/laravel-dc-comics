f3365cc8386d49c2247f1ca6074d87f161515acc
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-27 16:25:22 +0200

    created migration for db

--- database/migrations/2024_05_27_140426_create_comics_table.php
+++ database/migrations/2024_05_27_140426_create_comics_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 255);
+            $table->text('description')->nullable();
+            $table->string('thumb')->default('https://static.dc.com/2023-01/riddler%403x.png?w=1173');
+            $table->string('price', 10);
+            $table->string('series', 100);
+            $table->date('sale_date');
+            $table->string('type', 20);
             $table->timestamps();
         });
     }
