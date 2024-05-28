4f9253c85727a71b36cc45a1e4a1dcaa4f2d6858
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-28 19:43:53 +0200

    created template for create

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+@section('title', 'Inserisci un nuovo comic')
+
+@section('content')
+<section class="container text-light">
+    <form action="{{route('comics.store')}}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label for="title" class="form-label">Titolo</label>
+            <input type="text" class="form-control" id="title" name="title">
+        </div>
+        <div class="mb-3">
+            <label for="description" class="form-label">Descrizione</label>
+            <textarea class="form-control" id="description" rows="3" name="description"></textarea>
+        </div>
+        <div class="mb-3">
+            <label for="thumb" class="form-label">Immagine</label>
+            <input type="text" class="form-control" id="thumb" name="thumb">
+        </div>
+        <div class="mb-3">
+            <label for="price" class="form-label">Prezzo</label>
+            <input type="text" class="form-control" id="price" name="price">
+        </div>
+        <div class="mb-3">
+            <label for="series" class="form-label">Serie</label>
+            <input type="text" class="form-control" id="series" name="series">
+        </div>
+        <div class="mb-3">
+            <label for="type" class="form-label">Tipo</label>
+            <input type="text" class="form-control" id="type" name="type">
+        </div>
+        <button type="submit" class="btn btn-success">Crea</button>
+    </form>
+</section>
+@endsection
