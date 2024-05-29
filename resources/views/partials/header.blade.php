daefcc7c2dd04e01873393fe42fb49b92619435f
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-29 17:38:10 +0200

    update header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,26 +1,24 @@
-<header class="bg-white">
-    <div id="logo" class="text-center">
+<header class="container">
+    <nav class="py-3">
         <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Dc-logo">
-    </div>
-    <div id="main-menu">
-        <nav class="navbar-nav container navbar-light">
-            <ul class="list-unstyled d-flex justify-content-center gap-2 text-uppercase">
-                <li class="nav-item">
-                    <a class="nav-link {{Route::currentRouteName() == 'home' ? 'active' : ''}}" href="{{route('home')}}">
-                        Home
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{Route::currentRouteName() == 'comics.index' ? 'active' : ''}}" href="{{route('comics.index')}}">
-                        Comics
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{Route::currentRouteName() == 'comics.create' ? 'active' : ''}}"  href="{{route('comics.create')}}">
-                        Inserisci un comics
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    </div>
+        <ul class="d-flex">
+            <li>
+                <a class="nav-link {{Route::currentRouteName() == 'home' ? 'active' : ''}}" href="{{route('home')}}">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a class="nav-link {{Route::currentRouteName() == 'comics.index' ? 'active' : ''}}"
+                    href="{{route('comics.index')}}">
+                    Comics
+                </a>
+            </li>
+            <li>
+                <a class="nav-link {{Route::currentRouteName() == 'comics.create' ? 'active' : ''}}"
+                    href="{{route('comics.create')}}">
+                    Inserisci un comics
+                </a>
+            </li>
+        </ul>
+    </nav>
 </header>
